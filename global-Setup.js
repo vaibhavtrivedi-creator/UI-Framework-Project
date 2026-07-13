0b7df362e51bdd76efd125dc32089a6859ac4321
Vaibhav Trivedi <youremail@example.com>
2026-07-13 17:46:54 +0530

    Change in global setup file

--- global-Setup.js
+++ global-Setup.js
@@ -3,7 +3,7 @@ const { chromium } = require("@playwright/test");
 async function globalSetup() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(process.env.BASE_URL + "login");
+  await page.goto(`${process.env.BASE_URL}login`);
   await page.fill("#email", process.env.LOGIN_EMAIL);
   await page.fill("#password", process.env.LOGIN_PASSWORD);
   await page.click("#login-btn");
