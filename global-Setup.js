6244c4d9777eb0f13b2796347797c1d2d3f1cac1
Vaibhav Trivedi <youremail@example.com>
2026-07-14 17:44:42 +0530

    Change in global file for time waiting.

--- global-Setup.js
+++ global-Setup.js
@@ -9,9 +9,9 @@ async function globalSetup() {
   await page.fill("#email", process.env.LOGIN_EMAIL);
   await page.fill("#password", process.env.LOGIN_PASSWORD);
   await page.click("#login-btn");
-  await expect(
-    await page.getByRole("button", { name: "Logout" }),
-  ).toBeVisible();
+  await expect(await page.getByRole("button", { name: "Logout" })).toBeVisible({
+    timeout: 60000,
+  });
   await page.context().storageState({ path: "storageState.json" });
   await browser.close();
 }
